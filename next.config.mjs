f8bac5f1839d6b119a711e01ee4095463fc15893
Bektas Keldibayev <bektas.keldibayev@nu.edu.kz>
2025-02-13 15:09:43 +0500

    delete next.config.ts

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const nextConfig = {
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "eda.ru",
+      },
+      {
+        protocol: "https",
+        hostname: "s1.eda.ru",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
